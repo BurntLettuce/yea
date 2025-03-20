11815e511236d5d5021f55c19177c10a462cb397
BurntLettuce <97564572+BurntLettuce@users.noreply.github.com>
2025-03-20 19:41:15 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ swww-daemon &
 current_hour=$(date +%H)
 if [[ "$current_hour" -ge 0 && "$current_hour" -lt 12 ]]; then
     # From 00:00 to 11:59
-    swww img ~/wallpapers/wallpaper2.gif &
+    swww img ~/wallpapers/wallpaper2.jpg &
 else
     # From 12:00 to 23:59
-    swww img ~/wallpapers/wallpaper.jpg &
+    swww img ~/wallpapers/wallpaper1.jpg &
 fi
 
 nm-applet --indicator &
