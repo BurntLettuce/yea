1398536e07117614dde8b552583af2f831c0cf29
BurntLettuce <97564572+BurntLettuce@users.noreply.github.com>
2025-04-09 21:25:00 -0400

    Update configuration.nix

--- etc/nixos/hosts/scuffed/configuration.nix
+++ etc/nixos/hosts/scuffed/configuration.nix
@@ -1,1 +1,96 @@
+{ config, pkgs, inputs, lib, ... }:
 
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      inputs.sops-nix.nixosModules.sops
+    ];
+
+  networking.firewall.enable = true;
+
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+
+  sops.age.keyFile = "/home/ghostyytoastyy/.config/sops/age/keys.txt";
+
+  sops.secrets.example-key = { };
+  sops.secrets."myservice/my_subdir/my_secret" = {
+      owner = "ghostyytoastyy";
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.enable = true;
+
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.ghostyytoastyy = {
+    isNormalUser = true;
+    description = "main user";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  programs.firefox.enable = true;
+  programs.dconf.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    age
+    kitty
+    sops
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.openssh.enable = true;
+
+  users.users.ghostyytoastyy.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFouIyzSfXTYwET9IhNvxkRDejrKEA+Rw3yke0KF0crP ghosty>
+  ];
+  services.openssh.settings.PasswordAuthentication = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
+  system.stateVersion = "24.11";
+
+}
