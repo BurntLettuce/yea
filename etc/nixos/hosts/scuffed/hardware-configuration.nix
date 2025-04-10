82b3b75e9fd8a01708014f0f8e2e69a2cbebdf2d
BurntLettuce <97564572+BurntLettuce@users.noreply.github.com>
2025-04-10 17:28:34 -0400

    Update hardware-configuration.nix

--- etc/nixos/hosts/scuffed/hardware-configuration.nix
+++ etc/nixos/hosts/scuffed/hardware-configuration.nix
@@ -34,5 +34,5 @@
   # networking.interfaces.enp34s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirm>
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
