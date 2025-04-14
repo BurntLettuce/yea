8fc018903d9ce0c932596989b7476ab54e1f6179
BurntLettuce <97564572+BurntLettuce@users.noreply.github.com>
2025-04-14 02:56:14 -0400

    Create home.nix

---
+++ etc/nixos/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ghostyyistoasty";
+  home.homeDirectory = "/home/ghostyyistoasty";
+  home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    waybar
+    bibata-cursors
+    dracula-theme
+  ];
+
+  imports = [
+    ./modules/waybar.nix
+  ];
+
+  # Enable dconf (optional for GNOME users)
+  dconf.enable = true;
+ 
+  gtk = {
+     enable = true;
+     theme = {
+       name = "Dracula";
+       package = pkgs.dracula-theme;
+     };
+     cursorTheme = {
+       name = "Bibata-Modern-Ice";
+       package = pkgs.bibata-cursors;
+     };
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "nvim";
+    # BROWSER = "firefox";
+  };
+}
